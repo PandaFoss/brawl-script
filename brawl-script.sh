a5a136d37f8035122de07cd3f6591fc1ea596540
Panda Foss <maxi.fg13@gmail.com>
2020-03-11 16:29:54 -0300

    Añadidas mejoras menores

--- brawl-script.sh
+++ brawl-script.sh
@@ -3,28 +3,30 @@
 
 TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkaXNjb3JkX3VzZXJfaWQiOiI0MTkxNTg1NTE0NjY5OTk4MjEiLCJyZWFzb24iOiJwcm9iYW5kby10b2tlbi1icmF3bCIsInZlcnNpb24iOjEsImlhdCI6MTU4Mzc4MDQ2OH0.wxAkKDOyGmSWGKCVeqayq-CBs_BfZMzh-qAs18tHHFU"
 LINK="https://api.starlist.pro/v1/player?tag=$1"
+BOLD="\e[1m"
+NORMAL="\e[0m"
 
+NICK=$(curl --silent --header "Authorization: ${TOKEN}" ${LINK} | jq ".name")
+TROFEOS=$(curl --silent --header "Authorization: ${TOKEN}" $LINK | jq ".trophies")
+TROFEOS_MAX=$(curl --silent --header "Authorization: ${TOKEN}" ${LINK} | jq ".highestTrophies")
+LVL=$(curl --silent --header "Authorization: ${TOKEN}" ${LINK} | jq ".expLevel")
+EXP_TOTAL=$(curl --silent --header "Authorization: ${TOKEN}" ${LINK} | jq ".totalExp")
+WIN=$(curl --silent --header "Authorization: ${TOKEN}" ${LINK} | jq ".victories")
+SOLO_SUPERVIVENCIA=$(curl --silent --header "Authorization: ${TOKEN}" ${LINK} | jq ".soloShowdownVictories")
+DUO_SUPERVIVENCIA=$(curl --silent --header "Authorization: ${TOKEN}" ${LINK} | jq ".duoShowdownVictories")
+PELEA_ROBOTICA=$(curl --silent --header "Authorization: ${TOKEN}" ${LINK} | jq ".bestRoboRumbleTime")
+MEGABRAWLER=$(curl --silent --header "Authorization: ${TOKEN}" ${LINK} | jq ".bestTimeAsBigBrawler")
+BRAWLERS_DESBLOQUEADOS=$(curl --silent --header "Authorization: ${TOKEN}" ${LINK} | jq ".brawlersUnlocked")
 
-NICK=$(curl --silent --header "Authorization: $TOKEN" $LINK | jq ".name")
-TROFEOS=$(curl --silent --header "Authorization: $TOKEN" $LINK | jq ".trophies")
-TROFEOS_MAX=$(curl --silent --header "Authorization: $TOKEN" $LINK | jq ".highestTrophies")
-LVL=$(curl --silent --header "Authorization: $TOKEN" $LINK | jq ".expLevel")
-EXP_TOTAL=$(curl --silent --header "Authorization: $TOKEN" $LINK | jq ".totalExp")
-WIN=$(curl --silent --header "Authorization: $TOKEN" $LINK | jq ".victories")
-SOLO_SUPERVIVENCIA=$(curl --silent --header "Authorization: $TOKEN" $LINK | jq ".soloShowdownVictories")
-DUO_SUPERVIVENCIA=$(curl --silent --header "Authorization: $TOKEN" $LINK | jq ".duoShowdownVictories")
-PELEA_ROBOTICA=$(curl --silent --header "Authorization: $TOKEN" $LINK | jq ".bestRoboRumbleTime")
-MEGABRAWLER=$(curl --silent --header "Authorization: $TOKEN" $LINK | jq ".bestTimeAsBigBrawler")
-BRAWLERS_DESBLOQUEADOS=$(curl --silent --header "Authorization: $TOKEN" $LINK | jq ".brawlersUnlocked")
-	
-echo -e "\nNick: ${NICK}"
-echo "Trofeos: ${TROFEOS}"
-echo "Trofeos maximos: ${TROFEOS_MAX}"
-echo "Nivel: ${LVL}"
-echo "Experiencia total: ${EXP_TOTAL}"
-echo "Cantidad de victorias: ${WIN}"
-echo "Cantidad de victorias en Supervivencia Solo: ${SOLO_SUPERVIVENCIA}"
-echo "Cantidad de victorias en Supervivencia Duo: ${DUO_SUPERVIVENCIA}"
-echo "Mejor tiempo pelea robotica: ${PELEA_ROBOTICA}"
-echo "Mejor tiempo Megabrawler: ${MEGABRAWLER}"
-echo -e "Cantidad de brawlers desbloqueados: ${BRAWLERS_DESBLOQUEADOS}\n"
+echo ""
+echo -e "${BOLD}Nick:${NORMAL} ${NICK}"
+echo -e "${BOLD}Trofeos:${NORMAL} ${TROFEOS}"
+echo -e "${BOLD}Trofeos maximos:${NORMAL} ${TROFEOS_MAX}"
+echo -e "${BOLD}Nivel:${NORMAL} ${LVL}"
+echo -e "${BOLD}Experiencia total:${NORMAL} ${EXP_TOTAL}"
+echo -e "${BOLD}Cantidad de victorias:${NORMAL} ${WIN}"
+echo -e "${BOLD}Cantidad de victorias en Supervivencia Solo:${NORMAL} ${SOLO_SUPERVIVENCIA}"
+echo -e "${BOLD}Cantidad de victorias en Supervivencia Duo:${NORMAL} ${DUO_SUPERVIVENCIA}"
+echo -e "${BOLD}Mejor tiempo pelea robotica:${NORMAL} ${PELEA_ROBOTICA}"
+echo -e "${BOLD}Mejor tiempo Megabrawler:${NORMAL} ${MEGABRAWLER}"
+echo -e "${BOLD}Cantidad de brawlers desbloqueados:${NORMAL} ${BRAWLERS_DESBLOQUEADOS}\n"
